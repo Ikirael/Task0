0ebb252c5ffa52a077b79f0971a4aed8e8dd5905
Kiristokl <kzheltkov@gmail.com>
2022-08-10 17:16:40 +0300

    Вторая домашка

---
+++ Homework_2/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+using System;
+
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int second_n = number/10 % 10;
+Console.WriteLine($"Вторая цифра числа {number} => {second_n}");
---
+++ Homework_2/Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+using System;
+
+Console.WriteLine("Введите цифру дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day > 7 || day <= 0) Console.WriteLine("Такого дня недели не существует");
+else if (day == 6 || day == 7) Console.WriteLine("Да, это выходной");
+else Console.WriteLine("Нет, это не выходной");
