fc123ab0cf6ee83422d46db0e881b9107c11a343
Kiristokl <kzheltkov@gmail.com>
2022-08-16 18:22:29 +0300

    Homework 4 done!

---
+++ Homework_4/Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+using System;
+
+Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1; //нулевая степень числа равна 1
+
+Console.Write("Введите степень числа A: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i <= degree; ++i){
+    result = result * number;
+}
+
+Console.Write($"Число {number} в степени {degree} равно {result} ");
---
+++ Homework_4/Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+*/
+
+using System;
+
+int[] arr = new int[8];
+
+Console.WriteLine("Заполните массив: ");
+
+for(int i = 0; i < arr.Length; ++i){
+    Console.Write($"{i} элемент: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Вывод массива: [");
+for(int i = 0; i < arr.Length-1; ++i)
+    Console.Write($"{arr[i]}, ");
+
+Console.WriteLine($"{arr[7]}]");
