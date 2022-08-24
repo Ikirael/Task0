a03d0aa680aa0f17accca6d62db39a28b1c8529e
Kiristokl <kzheltkov@gmail.com>
2022-08-24 22:52:37 +0300

    Homework 5

---
+++ Homework_5/Task34/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; ++i)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for(int i = 0; i < array.Length; ++i)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+int QuaEvNumArray (int[] array)
+{
+
+    int cnt = 0; 
+    for(int i = 0; i < array.Length; ++i)
+    {
+        if( array[i]%2 == 0 ) ++cnt;
+    }
+    return cnt;
+}
+
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(size,100,999);
+PrintArray(array);
+
+Console.WriteLine($"В данном массиве {QuaEvNumArray(array)} четных элементов");
---
+++ Homework_5/Task36/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; ++i)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for(int i = 0; i < array.Length; ++i)
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();
+}
+
+int SumOddIndxArray (int[] array)
+{
+    int sum=0;
+    for(int i = 1; i < array.Length; i+=2)
+        sum = sum + array[i];
+
+    return sum;    
+    
+}
+
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(size,-100,100);
+PrintArray(array);
+
+Console.WriteLine($"Сумма элементов на нечетных индексах: {SumOddIndxArray(array)}");
