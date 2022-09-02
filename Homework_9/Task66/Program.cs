ecd93b2d7441b87f436664fa6d8457677ec9c3ae
Kiristokl <kzheltkov@gmail.com>
2022-09-02 19:35:09 +0300

    Homework 9

---
+++ Homework_9/Task66/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int RecSum(int M, int N, int sum)
+{
+    if(sum < N)
+    sum += RecSum(M,N,sum+1);
+
+    return sum;
+}
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a;
+if(N < M)
+{
+    a = N;
+    N = M;
+    M = a;
+}
+Console.WriteLine(RecSum(M,N,M+1));
